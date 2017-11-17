55ae5bc535875a3d28ef34ff241d9635303ab950
Anil Madhavapeddy <anil@recoil.org>
2017-11-17 20:16:03 +0000

    add jbuilder workspace-based travis tests

---
+++ .docker.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+DISTRO=${DISTRO:-alpine-3.6}
+VERSIONS=${OCAML_VERSIONS:-4.04.2 4.05.0 4.06.0}
+
+set -ex
+# TODO opam2 depext
+case $DISTRO in
+alpine-*) sudo apk add m4 ;;
+debian-*) sudo apt -y install m4 pkg-config ;;
+ubuntu-*) sudo apt -install m4 pkg-config ;;
+esac
+
+cd /home/opam/src
+export OPAMYES=1
+export OPAMJOBS=3
+opam install --deps-only .
+rm -f jbuild-workspace.dev
+for v in $VERSIONS; do
+  echo "(context ((switch $v)))" >> jbuild-workspace.dev
+  opam install --deps-only -t --switch $v .
+done
+
+jbuilder runtest --workspace jbuild-workspace.dev
+rm -f jbuild-workspace.dev
---
+++ .travis.sh
@@ -0,0 +1,1 @@
+docker run -it -v `pwd`:/home/opam/src ocaml/opam2:${DISTRO}-ocaml /home/opam/src/.docker.sh
