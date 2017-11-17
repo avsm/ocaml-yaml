8d2e2888abca8e06ee651ce5036022a70bcc713b
Anil Madhavapeddy <anil@recoil.org>
2017-11-17 20:20:52 +0000

    fix travis

--- .travis.sh
+++ .travis.sh
@@ -1,1 +1,1 @@
-docker run -it -v `pwd`:/home/opam/src ocaml/opam2:${DISTRO}-ocaml /home/opam/src/.docker.sh
+docker run -it -e OCAML_VERSIONS="${OCAML_VERSIONS}" -v `pwd`:/home/opam/src ocaml/opam2:${DISTRO}-ocaml /home/opam/src/.docker.sh
