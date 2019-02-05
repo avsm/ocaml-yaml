f799d4d2cc54e30c3fc5557b368b92a73b088643
Anil Madhavapeddy <anil@recoil.org>
2019-02-05 01:58:43 +0100

    use dune instead of jbuilder

--- .docker.sh
+++ .docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-DISTRO=${DISTRO:-alpine-3.6}
-VERSIONS=${OCAML_VERSIONS:-4.04.2 4.05.0 4.06.0}
+DISTRO=${DISTRO:-alpine}
+VERSIONS=${OCAML_VERSIONS:-4.04 4.05 4.06 4.07}
 
 set -ex
 # TODO opam2 depext
@@ -10,19 +10,18 @@ debian-*) sudo apt update; sudo apt -y install m4 pkg-config ;;
 ubuntu-*) sudo apt update; sudo apt -y install m4 pkg-config ;;
 esac
 
-
 sudo chown -R opam /home/opam/src
 cd /home/opam/src
 export OPAMYES=1
 export OPAMJOBS=3
 opam repo set-url default https://github.com/ocaml/opam-repository.git
 opam install --deps-only .
-rm -f jbuild-workspace.dev
+rm -f dune-workspace.dev
 for v in $VERSIONS; do
-  echo "(context ((switch $v)))" >> jbuild-workspace.dev
+  echo "(context ((switch $v)))" >> dune-workspace.dev
   opam pin add crowbar https://github.com/stedolan/crowbar.git --switch $v
   opam install --deps-only -t --switch $v .
 done
 
-jbuilder runtest --workspace jbuild-workspace.dev
-rm -f jbuild-workspace.dev
+dune runtest --workspace dune-workspace.dev
+rm -f dune-workspace.dev
