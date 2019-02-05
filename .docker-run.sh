af14292431cb3098f91c511e3ae52b4b69f6e87a
Anil Madhavapeddy <anil@recoil.org>
2019-02-05 01:28:32 +0100

    fix CI

--- .docker-run.sh
+++ .docker-run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-DISTRO=${DISTRO:-alpine-3.6}
-OCAML_VERSIONS=${OCAML_VERSIONS:4.04.2 4.05.0 4.06.0}
-docker run -it -e DISTRO=${DISTRO} -e OCAML_VERSIONS="${OCAML_VERSIONS}" -v `pwd`:/home/opam/src ocaml/opam2:${DISTRO}-ocaml /home/opam/src/.docker.sh
+DISTRO=${DISTRO:-alpine}
+OCAML_VERSIONS=${OCAML_VERSIONS:4.04 4.05 4.06 4.07}
+docker run -it -e DISTRO=${DISTRO} -e OCAML_VERSIONS="${OCAML_VERSIONS}" -v `pwd`:/home/opam/src ocaml/opam2:${DISTRO} /home/opam/src/.docker.sh
